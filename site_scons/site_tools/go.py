ae608a3b221b97c0140cea2d6fc5a62c1ceaa0f0
Michael Lauer <michael@mrlauer.com>
2012-02-12 08:49:15 -0500

    better warning handling

--- site_scons/site_tools/go.py
+++ site_scons/site_tools/go.py
@@ -30,7 +30,7 @@ def generate(env):
             src_suffix = '$GOOBJSUFFIX',
             src_builder = ['GoObject'])
     golibbuild = Builder(action = '$GOLINKER $_GOLINKFLAGS -o $TARGET $SOURCES',
-            suffix = '.a',
+            suffix = '$GOLIBSUFFIX',
             src_suffix = '$GOOBJSUFFIX',
             src_builder = ['GoObject'])
 
@@ -62,6 +62,7 @@ def generate(env):
     env.SetDefault(GOCC = '${GOARCHCHAR}c')
     env.SetDefault(GOLINKER = '${GOARCHCHAR}l')
     env.SetDefault(GOOBJSUFFIX = '.${GOARCHCHAR}')
+    env.SetDefault(GOLIBSUFFIX = '${LIBSUFFIX}')
     env.Append(BUILDERS = { 'GoObject' : gobld })
     env.Append(BUILDERS = { 'GoPack' : gopackbld })
     env.Append(BUILDERS = { 'GoExe' : goexebuild })
@@ -69,8 +70,9 @@ def generate(env):
     env.SetDefault(GOALLPKGPATH = '$GOINCPATH:$GOPKGPATH')
     env['_make_gocflags'] = _make_gocflags
     env.SetDefault(GOCFLAGS = '${ _make_gocflags(__env__) }')
+    env.SetDefault(GOLINKFLAGS = '')
     env.SetDefault(_GOINFLAGS = '$( ${_concat( "-I", GOINCPATH, "", __env__, RDirs, TARGET, SOURCE ) } $)')
-    env.SetDefault(_GOLINKFLAGS = '$( ${_concat( "-L", GOINCPATH, "", __env__, RDirs, TARGET, SOURCE ) } $)')
+    env.SetDefault(_GOLINKFLAGS = '$( ${_concat( "-L", GOINCPATH, "", __env__, RDirs, TARGET, SOURCE ) } $) $GOLINKFLAGS')
     env.Append(SCANNERS = goscan)
 
 def exists(env):
@@ -80,18 +82,18 @@ def exists(env):
 def _goimport_scan(node, env, path, arg = None):
     if callable(path):
         path = path()
-#    if not (node.exists() or node == node.srcnode()):
-#        # generated nodes don't necessarily exist yet, so can't be scanned
-#        print node, "does not exist"
-#        return []
-    proc = subprocess.Popen('godeps', stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-    output, _ = proc.communicate(node.get_text_contents())
+    try:
+        proc = subprocess.Popen('godeps', stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        output, _ = proc.communicate(node.get_text_contents())
+    except OSError, e:
+        SCons.Warnings.warn(SCons.Warnings.Warning, "Error executing godeps: %s" % e)
+        return []
 
     topdir = env.Dir('#').abspath
 
     results = []
     for s in output.split():
-        arname = s + '.a'
+        arname = s + env.subst('$GOLIBSUFFIX')
         for p in path:
             pname = os.path.join(topdir, str(p), arname)
             if os.path.exists(pname):
@@ -100,6 +102,9 @@ def _goimport_scan(node, env, path, arg = None):
             if File(pname).has_builder():
                 results.append(pname)
                 break
+        else:
+            SCons.Warnings.warn(SCons.Warnings.DependencyWarning, "Could not find go dependency %s imported in %s" 
+                    % (arname, str(node)))
     return results
 
 def _goimport_pathfn(env, *args):
